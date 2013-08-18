b96824a2c5a104ca51b2079cb52930d3e5f9aca9
cotrim149 <cotrim149@gmail.com>
2013-08-17 22:13:24 -0300

    Commit inicial. Criado projeto MamaProject. Classe aluno adicionada ao projeto.

---
+++ src/org/cotrim/MamaProject/Aluno/Aluno.java
@@ -0,0 +1,89 @@
+package org.cotrim.MamaProject.Aluno;
+
+public class Aluno {
+
+	private String nome;
+	private int idade;
+	private String serie;
+	private String turma;
+	private String turno;
+	private String professora;
+	
+	
+	public Aluno(){
+		setNome("");
+		setIdade(0);
+		setSerie("");
+		setTurma("");
+		setTurno("");
+		setProfessora("");
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public String getSerie() {
+		return serie;
+	}
+
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+
+	public String getTurma() {
+		return turma;
+	}
+
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+
+	public String getTurno() {
+		return turno;
+	}
+
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+
+	public String getProfessora() {
+		return professora;
+	}
+
+
+	public void setProfessora(String professora) {
+		this.professora = professora;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", serie=" + serie
+				+ ", turma=" + turma + ", turno=" + turno + ", professora="
+				+ professora + "]";
+	}
+	
+	
+}
